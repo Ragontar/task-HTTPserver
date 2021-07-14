a40e704cf8cc60810d02eced54fa0fb0bf0cb1bb
reaver <ragontar@gmail.com>
2021-07-14 17:49:50 +0300

    Added project structure and dockerfile

---
+++ go.mod
@@ -0,0 +1,3 @@
+module task-httpserver
+
+go 1.16
--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"task-httpserver/pkg/server"
+)
 
 func main() {
 	fmt.Println("Hello world!")
+	server.Foo()
 }
